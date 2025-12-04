0e56386405d27e1c19bd30b77ba4678a98f6a310
sagarbhat-virtuele <sagar.r@virtuele.us>
2025-12-04 18:25:02 +0530

    updated booking service

--- app/src/main/java/ticket.booking/entities/User.java
+++ app/src/main/java/ticket.booking/entities/User.java
@@ -5,6 +5,47 @@ import java.util.List;
 public class User {
 
     private String name;
+
+    public List<Ticket> getTicketsBooked() {
+        return ticketsBooked;
+    }
+
+    public void setTicketsBooked(List<Ticket> ticketsBooked) {
+        this.ticketsBooked = ticketsBooked;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public void setHashedPassword(String hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
     private String password;
     private String hashedPassword;
     private List<Ticket> ticketsBooked;
--- app/src/main/java/ticket.booking/service/UserBookingService.java
+++ app/src/main/java/ticket.booking/service/UserBookingService.java
@@ -6,6 +6,7 @@ import ticket.booking.entities.User;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -27,4 +28,21 @@ public class UserBookingService {
 
 
     }
+    public Boolean loginUser(){
+        Optional<User> foundUser = userList.stream().filter(user1 -> {
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
+        }).findFirst();
+        return foundUser.isPresent();
+    }
+
+    public Boolean signUp(User user1){
+        try{
+            userList.add(user1);
+            saveUserListToFile();
+            return Boolean.TRUE;
+        }catch (IOException ex){
+            return Boolean.FALSE;
+        }
+    }
+
 }
